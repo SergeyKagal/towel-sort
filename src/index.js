2dded3519532d348be3c0b5b9602c2139c1de43c
SergeyKagal <3461995@gmail.com>
2021-03-14 01:51:53 +0300

    init: create index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var arr=matrix;
+
+  if(!arr) return [];
+   
+   var str=[];
+   
+   for (var i=0;i<arr.length;i++){
+       if(i%2!=0)arr[i].reverse();
+   }
+   var stack=[];
+
+for(var i=0;i<arr.length;i++)
+for(var j=0;j<arr[i].length;j++) stack.push(arr[i][j]);
+   
+   
+
+
+   
+  return stack;
 }
